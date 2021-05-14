520f6de1aa991d45374276f49fb87493450e3304
Diana Shagiakhmetova <dshagiakh@gmail.com>
2021-05-15 01:06:58 +0200

    feat: add simple controlled form without validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FormWithValidation from "./components/FormWithValidation";
+import FormWithoutValidation from "./components/FormWithoutValidation";
 
 function App() {
   return (
     <div className="App">
       <FormWithValidation />
+      <FormWithoutValidation />
     </div>
   );
 }
---
+++ src/components/FormWithoutValidation.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function SimpleForm() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Form submitted successfully!`);
+    setForm({ name: "", email: "", password: "" });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input type="text" name="name" value={form.name} onChange={handleChange} />
+      </div>
+
+      <div>
+        <label>Email:</label>
+        <input type="email" name="email" value={form.email} onChange={handleChange} />
+      </div>
+
+      <div>
+        <label>Password:</label>
+        <input type="password" name="password" value={form.password} onChange={handleChange} />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default SimpleForm;
