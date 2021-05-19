f3f77d61caed70b2ec843a68d46ce5aa7220fffe
Diana Shagiakhmetova <dshagiakh@gmail.com>
2021-05-20 01:42:17 +0200

    feat: add trim() usage example for practice

--- src/components/FormWithoutValidation.jsx
+++ src/components/FormWithoutValidation.jsx
@@ -20,21 +20,45 @@ function SimpleForm() {
     setForm({ name: "", email: "", password: "" });
   };
 
+  // trim() function:
+
+  const input = "            Hello, world!                  ";
+  const trimmed = input.trim();
+
+  console.log(trimmed); // "Hello, world!"
+
+  //
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Name:</label>
-        <input type="text" name="name" value={form.name} onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+        />
       </div>
 
       <div>
         <label>Email:</label>
-        <input type="email" name="email" value={form.email} onChange={handleChange} />
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+        />
       </div>
 
       <div>
         <label>Password:</label>
-        <input type="password" name="password" value={form.password} onChange={handleChange} />
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+        />
       </div>
 
       <button type="submit">Submit</button>
